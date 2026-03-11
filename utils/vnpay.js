298384a8a97745f1f17176303998959cd98f9bad
hminh <hminh250104@gmail.com>
2026-03-12 04:03:37 +0700

    fix encoding

--- utils/vnpay.js
+++ utils/vnpay.js
@@ -37,7 +37,7 @@ function createPaymentUrl(params) {
 
   vnp_Params = sortObject(vnp_Params);
 
-  const signData = qs.stringify(vnp_Params, { encode: true });
+  const signData = qs.stringify(vnp_Params, { encode: false });
 
   const hashSecret = (process.env.VNP_HASH_SECRET || "").trim();
   const vnpUrl = (process.env.VNP_URL || "").trim();
@@ -63,7 +63,7 @@ function verifyIpnSignature(vnp_Params) {
 
   const sortedParams = sortObject(vnp_Params);
 
-  const signData = qs.stringify(sortedParams, { encode: true });
+  const signData = qs.stringify(sortedParams, { encode: false });
 
   const hashSecret = (process.env.VNP_HASH_SECRET || "").trim();
   const hmac = crypto.createHmac("sha512", hashSecret);
